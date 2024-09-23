3be9ae540f1e813819e9883c2fd7df2c46b2056a
Patrick Connolly <patrick.c.connolly@gmail.com>
2024-09-23 00:29:57 -0400

    Added withRouter decorator to Header in client-admin, to avoid bug.

--- stories/client-admin/Header.stories.js
+++ stories/client-admin/Header.stories.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import Header from '../../polis/client-admin/src/components/landers/lander-header'
 import { withThemeUi } from '../../.storybook/decorators'
+import { withRouter } from 'storybook-addon-remix-react-router';
 
 export default {
   title: 'Client-Admin/Header',
   component: Header,
-  decorators: [withThemeUi],
+  decorators: [withRouter, withThemeUi],
 }
 
 const Template = (args) => <Header {...args} />
