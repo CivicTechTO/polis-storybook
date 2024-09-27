31e8756da552c9884b9e82b6160125725616bd04
Patrick Connolly <patrick.c.connolly@gmail.com>
2024-09-26 21:55:40 -0400

    Added TidCarousel stories.

---
+++ stories/client-participation/TidCarousel.stories.js
@@ -0,0 +1,78 @@
+import React from 'react'
+import { action } from '@storybook/addon-actions'
+import TidCarousel from '../../polis/client-participation/vis2/components/tidCarousel'
+import Strings from '../../polis/client-participation/js/strings/en_us'
+
+import commentsData from '../../.storybook/data/3ntrtcehas-comments.json'
+
+commentsData.sort((a,b) => a.tid - b.tid)
+
+const pluckNBetweenLowerUpper = (n, lower, upper) => {
+  let numbers = []
+  while (numbers.length < n) {
+    let candidate = Math.floor(Math.random() * (upper - lower)) + (lower + 1)
+    if (!numbers.includes(candidate)) {
+      numbers.push(candidate)
+    }
+  }
+  // Ascending integer sort.
+  numbers.sort((a, b) => a - b)
+  
+  return numbers
+}
+
+export default {
+  title: 'Client-Participation/TidCarousel',
+  component: TidCarousel
+}
+
+const Template = (args) => <TidCarousel {...args} />
+
+export const Default = Template.bind({})
+Default.args = {
+  selectedTidCuration: 1,
+  commentsToShow: commentsData.slice(10,20),
+  selectedComment: null,
+  Strings,
+  handleCommentClick: (e) => action("Clicked"),
+  Strings: Strings,
+}
+
+// TODO: Load dataset with hundreds/thousands of comments.
+//export const DoubleToTripleDigits = Template.bind({})
+//DoubleToTripleDigits.args = {
+//  ...Default.args,
+//  commentsToShow: commentsData.slice(95,105),
+//}
+
+export const StatementSelected = Template.bind({})
+StatementSelected.args = {
+  ...Default.args,
+  selectedComment: { tid: commentsData[11].tid }
+}
+
+export const FewStatements = Template.bind({})
+FewStatements.args = {
+  ...Default.args,
+  commentsToShow: commentsData.slice(10,15),
+}
+
+export const Pagination = Template.bind({})
+Pagination.args = {
+  ...Default.args,
+  commentsToShow: commentsData.slice(10,40),
+}
+
+export const UpToDoubleDigits = Template.bind({})
+UpToDoubleDigits.args = {
+  ...Default.args,
+  commentsToShow: [
+    ...pluckNBetweenLowerUpper(10, 0, commentsData.length-1).map(i => commentsData[i])
+  ]
+} 
+
+export const NoGroupSelectedSoHidden = Template.bind({})
+NoGroupSelectedSoHidden.args = {
+  ...Default.args,
+  selectedTidCuration: null
+}
