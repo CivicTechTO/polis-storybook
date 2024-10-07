4c127be8ecc292bec0e7b63f6309bb22e76c0d9c
Patrick Connolly <patrick.c.connolly@gmail.com>
2024-10-07 18:51:34 -0400

    Testing to get pathname's working for branch switcher.

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,6 +1,9 @@
 /** @type { import('@storybook/react').Preview } */
 const preview = {
   parameters: {
+    branches: {
+      hostname: "127.0.0.1:8080/storybook-bundle",
+    },
     controls: {
       matchers: {
         color: /(background|color)$/i,
