18e10c5d0baf85273e83bf75f9fc6b0dce2c0d79
Patrick Connolly <patrick.c.connolly@gmail.com>
2024-10-07 21:30:31 -0400

    Removed localhost debug for multi-branch.

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -2,9 +2,6 @@
 const isGithubBuild = !!process.env["GITHUB_REPOSITORY"]
 const preview = {
   parameters: {
-    branches: {
-      hostname: "127.0.0.1:8080/storybook-bundle",
-    },
     controls: {
       matchers: {
         color: /(background|color)$/i,
