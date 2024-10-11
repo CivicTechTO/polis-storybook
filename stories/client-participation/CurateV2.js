d06c30878c7f30a90c843fcbf91d6d16f0bc18ab
Patrick Connolly <patrick.c.connolly@gmail.com>
2024-10-11 02:52:23 -0400

    Extracted TidCarouselV2 and CurateV2 components into own files.

---
+++ stories/client-participation/CurateV2.js
@@ -0,0 +1,76 @@
+import React from 'react'
+import * as globals from '../../codebases/compdem/client-participation/vis2/components/globals'
+
+export const CurateV2Button = ({isSelected, onCurateButtonClick, style, children}) => {
+  const colors = {
+    polisBlue: "#03a9f4",
+    darkGray: "rgb(100,100,100)",
+    lightGray: "rgb(235,235,235)",
+  }
+  const buttonStyle = {
+    ...style,
+    border: 0,
+    cursor: "pointer",
+    borderRadius: 4,
+    fontSize: 14,
+    padding: "6px 12px",
+    letterSpacing: 0.75,
+    // fontWeight: isSelected ? 700 : 500,
+    textShadow: isSelected ? "0 0 .65px #fff" : null,
+    backgroundColor: isSelected ? colors.polisBlue : colors.lightGray,
+    color: isSelected ? "white" : colors.darkGray,
+  }
+  return (
+    <button style={buttonStyle} role="tab" aria-selected={isSelected} onClick={onCurateButtonClick}>
+      {children}
+    </button>
+  )
+}
+
+const CurateV2 = ({selectedTidCuration, handleCurateButtonClick = () => {}, math}) => {
+  const GROUP_COUNT = math["group-clusters"].length
+  const styles = {
+    container: {
+      display: "flex",
+      flexDirection: "column",
+      rowGap: 5,
+    },
+    groupContainer: {
+      display: "flex",
+      gap: 5,
+    },
+    groupButton: {
+      flex: 1,
+    },
+    majorityButton: {
+      width: "100%",
+    },
+  }
+  const groups = ['A', 'B', 'C', 'D', 'E']
+  
+  return(
+    <div style={styles.container} role="tablist">
+      <div style={styles.groupContainer}>
+        {groups.slice(0, GROUP_COUNT).map((groupLabel, index) => (
+          <CurateV2Button
+            key={groupLabel}
+            onCurateButtonClick={() => handleCurateButtonClick(index)}
+            isSelected={selectedTidCuration === index}
+            style={styles.groupButton}
+          >
+            {groupLabel}
+          </CurateV2Button>
+        ))}
+      </div>
+      <CurateV2Button
+        onCurateButtonClick={() => handleCurateButtonClick(globals.tidCuration.majority)}
+        isSelected={selectedTidCuration === globals.tidCuration.majority}
+        style={styles.majorityButton}
+      >
+        Diverse Majority Opinion
+      </CurateV2Button>
+    </div>
+  )
+}
+
+export default CurateV2
---
+++ stories/client-participation/TidCarouselV2.js
@@ -0,0 +1,97 @@
+import React from 'react'
+import { animated, useTransition } from '@react-spring/web'
+import useMeasure from 'react-use-measure'
+
+export const TidCarouselButton = ({ label, isShown, isSelected, handleClick, containerWidth }) => {
+  const transition = useTransition(isShown, {
+    from: {
+      width: 0,
+      marginRight: 0,
+      opacity: 1,
+    },
+    enter: {
+      width: containerWidth/5-5,
+      marginRight: 0,
+      opacity: 1,
+    },
+    leave: {
+      width: 0,
+      marginRight: -5,
+      opacity: 0,
+    },
+  })
+  return (
+    transition((style, isShownTransition) => (
+      isShownTransition && <animated.button
+        onClick={handleClick}
+        style={{
+          width: style.width,
+          height: 30,
+          marginRight: style.marginRight,
+          // fontSize changes seem slow. Scale span instead.
+          // fontSize: style.fontSize,
+          opacity: style.opacity,
+          padding: 0,
+          border: 0,
+          cursor: "pointer",
+          overflow: "hidden",
+          borderRadius: 4,
+          fontWeight: isSelected ? 700 : 300,
+          backgroundColor: isSelected ? "#03a9f4" : "rgb(235,235,235)",
+          color: isSelected ? "white" : "rgb(0,0,0)",
+        }}>
+        <span style={{
+          // 1s is rought estimate, but react-spring uses forces, not duration.
+          transition: "transform 1s ease-in-out",
+          transform: isShown ? "scaleX(1)" : "scaleX(0.5)",
+          // Needed in order to transform.
+          display: "inline-block",
+        }}>
+          {label}
+        </span>
+      </animated.button>
+    ))
+  )
+}
+
+const TidCarouselV2 = ({
+  selectedTidCuration,
+  allComments,
+  commentsToShow,
+  selectedComment,
+  handleCommentClick,
+  Strings,
+}) => {
+  if ( selectedTidCuration === null ) return null
+  const [ref, bounds] = useMeasure()
+
+  allComments = allComments.sort((a, b) => a.tid - b.tid)
+  const commentsToShowTids = commentsToShow.map(c => c.tid)
+
+  // ref not available on first render, so only render map after bounds exists.
+  return (
+    <div ref={ref} style={{
+      display: "flex",
+      flex: 1,
+      width: "100%",
+      height: 65,
+      paddingX: 0,
+      gap: 5,
+      rowGap: 5,
+      flexWrap: "wrap",
+    }}>
+      {!bounds.width || allComments.map((c, i) => (
+        <TidCarouselButton
+          containerWidth={bounds.width}
+          id={c.tid}
+          label={c.tid}
+          handleClick={() => handleCommentClick(c)}
+          isSelected={selectedComment && selectedComment.tid === c.tid}
+          isShown={commentsToShowTids.includes(c.tid)}
+        />
+      ))}
+    </div>
+  )
+}
+
+export default TidCarouselV2
